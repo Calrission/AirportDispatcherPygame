ef1f1d29d700f23be81007e0ea657a67d201c5f2
Kavrese <kavrese@gmail.com>
2023-01-29 12:47:15 +0300

    update scenario files and detail callback message for CommandLand.py

--- Commands/CommandLand.py
+++ Commands/CommandLand.py
@@ -2,6 +2,7 @@ from Screens.Game import AircraftController
 from Commands.Command import Command
 from MultiLineText import MultiLineText
 from Terminals.AircraftsTerminal import AircraftsTerminal
+from Sprites.FlyTransport import StatusFlyTransport
 
 
 class CommandLand(Command):
@@ -29,6 +30,12 @@ class CommandLand(Command):
             id = self._params[0]
             way = self._params[1]
             aircraft = controller.get_aicraft(id)
+            if aircraft is None:
+                out.add_text(f"Самолет с ID={id} не найден")
+                return
+            if aircraft.status != StatusFlyTransport.FLY:
+                out.add_text("Данный самолет уже не находиться в воздухе или больше не может приземлиться")
+                return
             if aircraft.animation is None:
                 controller.landing(aircraft, way)
                 terminal.remove(id)
