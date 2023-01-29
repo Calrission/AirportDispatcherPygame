09f22dc7c0fdd7a8528b889cfef729f0e25b68b7
PushkinC <zolotarev_d_e@mail.ru>
2023-01-29 23:49:24 +0300

    fix return from AboutAs

--- Screens/Levels.py
+++ Screens/Levels.py
@@ -6,8 +6,3 @@ class Levels(Menu):
     def __init__(self, x: int, y: int, padding: int, surface: pygame.Surface, background: str):
         super().__init__(x, y, padding, surface, background)
 
-    def parse_event(self, e):
-        super().parse_event(e)
-        if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_BACKSPACE:
-                self._callbacks[-1]()
