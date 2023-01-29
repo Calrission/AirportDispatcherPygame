441dd0a529cd4da2975620c95fefd635388e58e6
PushkinC <zolotarev_d_e@mail.ru>
2023-01-29 18:20:59 +0300

    add AboutAs.py

--- App.py
+++ App.py
@@ -9,6 +9,7 @@ from Scenario.Scenario import Scenario
 from Screens.Menu import Menu
 from Screens.Settings import Settings
 from Screens.Levels import Levels
+from Screens.AboutAs import AboutAs
 
 
 class App:
@@ -31,6 +32,7 @@ class App:
 
         self.menu = Menu(100, 100, 100, self.surface, 'Sprites/Menu/Level_background.png')
         self.levels = Levels(100, 100, 100, self.surface, 'Sprites/Menu/Level_background.png')
+        self.aboutAs = AboutAs(80, screen_height // 2, 100, self.surface, 'Sprites/Menu/Level_background.png')
         self.settings = Settings(100, 100, 100, self.surface, 'Sprites/Menu/Level_background.png', 'Settings.set')
 
         self.now_screen: Screen = self.menu
@@ -39,7 +41,7 @@ class App:
             Game(self.surface, self.load_level('Scenario/debagScenario.scen'), self.clock)))
         self.menu.append_option('Уровни', lambda: self.change_screen(self.levels))
         self.menu.append_option('Настройки', lambda: self.change_screen(self.settings))
-        self.menu.append_option('О программе', lambda: print('Экран о программе'))
+        self.menu.append_option('О программе', lambda: self.change_screen(self.aboutAs))
         self.menu.append_option('Выход', lambda: sys.exit())
 
         self.levels.append_option('Уровень-1', lambda: self.change_screen(
@@ -53,6 +55,8 @@ class App:
         self.settings.append_option('Громкость музыки:', lambda: self.change_screen(self.settings))
         self.settings.append_option('Громкость звуков:', lambda: self.change_screen(self.settings))
         self.settings.append_option('Вернуться', lambda: self.change_screen(self.menu))
+
+        self.aboutAs.append_option('Вернуться', lambda: self.change_screen(self.menu))
 
     def tick(self):
         self.surface.fill("black")
---
+++ Screens/AboutAs.py
@@ -0,0 +1,38 @@
+from Screens.Menu import Menu
+import pygame
+from const import screen_width, screen_height
+
+
+class AboutAs(Menu):
+    def __init__(self, x: int, y: int, padding: int, surface: pygame.Surface, background: str):
+        super().__init__(x, y, padding, surface, background)
+        texts = {
+            'text_school': 'Проект "Диспетчер аэропорта" создан для Академии Яндекса',
+            'text_autor': 'Авторы: Артемий Струков, Золотарёв Данил',
+            'text_programmer': 'Разработчики: Артемий Струков, Золотарёв Данил',
+            'text_design': 'Дезайн: Артемий Струков, Золотарёв Данил',
+            'text_supervisor': 'Руководитель: Симонов Сергей Иванович',
+            'text_phone': 'Телефон: 8(968)434-95-65',
+            'test_email': 'e-mail: zolotarev_d_e@mail.ru',
+        }
+
+        self.rects = {}
+        self.font1 = pygame.font.Font('Fonts/AmaticSC-Bold.ttf', 50)
+        self.padding1 = 75
+
+        for key, item in texts.items():
+            self.rects[key] = self.font1.render(item, True, (0, 0, 0))
+
+
+
+
+
+    def draw(self):
+        super().draw()
+        i = 0
+        for key, item in self.rects.items():
+            rect = item.get_rect()
+            rect.center = (screen_width // 2 + 100, 50 + i * self.padding1)
+            self.surface.blit(item, rect)
+            i += 1
+
